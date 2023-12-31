673881e30965c3e18c48c3c8cfdc49353d5eb745
Aymen Cherkani <cherkaniaymen1@gmail.com>
2023-12-31 13:29:14 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// vite.config.js
 export default defineConfig({
-  plugins: [react()]
-})
+  base: "/test_deploiment2/",
+  plugins: [react()],
+});
