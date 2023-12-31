496a2ac66158a96117b47f8e7692c9b571f6adf8
Aymen Cherkani <cherkaniaymen1@gmail.com>
2023-12-31 13:13:47 +0100

    Configure deployment for GitHub Pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "node_modules/preline/dist/*.js",
-    
   ],
   theme: {
     extend: {},
     fontFamily: {
-      body:["Poppins"]
-    }
+      body: ["Poppins"],
+    },
+  },
+  build: {
+    outDir: "dist",
   },
   plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
 };
